07c128d18296de14dfe7c57043190bb7f16bff5d
Yi <hoon_5083@naver.com>
2021-12-12 18:40:02 +0900

    [2798] <complete> (without comment)

--- BaekJoon_2798.c
+++ BaekJoon_2798.c
@@ -5,6 +5,9 @@ int n = 0;
 int m = 0;
 int buf = 0;
 int input[100];
+int combination[1000000];
+int count = 0;
+int min = 100000;
 
 int main(){
   scanf("%d %d", &n, &m);
@@ -20,5 +23,21 @@ int main(){
       }
     }
   }
+  for(int i = 0;i<n;i++){
+    for(int j = 0;j<n;j++){
+      for(int k = 0;k<n;k++){
+        if((i != j)&&(j != k)&&(k != i)){
+          combination[count] = input[i]+input[j]+input[k];
+          count++;
+        }
+      }
+    }
+  }
+  for(int i = 0; i<count; i++){
+    if(((m - combination[i]) < min)&&((m - combination[i])>=0)){
+      min = m - combination[i];
+    }
+  }
+  printf("%d", m - min);
   return 0;
 }
